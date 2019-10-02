0019543fb458678b2d8db49204b15b6d804d2738
gusmatiasm <gusk8erpro@gmail.com>
2019-10-02 11:58:41 -0300

    first js

---
+++ playground/script/main.js
@@ -0,0 +1,20 @@
+console.log("Starting javascript...");
+var myName = "Gustavo Morales";
+var age = 30;
+var ignasiAge = 32;
+var ageDiff = age - ignasiAge;
+if (age > 21) {
+    console.log("You are older than 21")
+}
+else {
+    console.log("You are not older than 21")
+}
+if (age > ignasiAge) {
+    console.log("Ignasi is younger than you")
+}
+else if (age < ignasiAge) {
+    console.log("Ignasi is older than you")
+}
+else {
+    console.log("You have the same age as Ignasi")
+}
